da28e2ba51cdfcc394c0c97aa91dba77d6e4cf4d
JosephBizra <j.bizra@gmail.com>
2019-07-02 01:32:49 -0500

    finished

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -13,4 +13,52 @@ var connection = mysql.createConnection({
     // Your password
     password: "qwer2wsx",
     database: "bamazonDB"
+});
+
+connection.connect(function(error){
+  if(error) throw error;
+  console.log("successfully connected to mysql");
+});
+ 
+connection.query('SELECT * FROM products', function (error, results, fields) {
+  if (error) throw error;
+  console.table(results);
+  prompt();
+});
+
+function prompt(){
+ inquirer
+  .prompt([
+    {
+      type: "input",
+      name: "itemId",
+      message: "What is the id of the product you want to buy?"
+    },
+    {
+      type: "input",
+      name: "itemQuantity",
+      message: "How much of this item do you want?"
+    },
+  ])
+  .then(function(answers){
+    var itemId = answers.itemId;
+    var itemQuantity = answers.itemQuantity;
+
+    connection.query("SELECT * FROM products WHERE id = " + itemId, function (error, results, fields) {
+      if (error) throw error;
+      if(itemQuantity > results[0].stock_quantity){
+        console.log("Insufficient quantity!");
+        connection.end();
+      } else {
+        var currentStock = results[0].stock_quantity - itemQuantity;
+        var price = results[0].price;
+
+        connection.query("UPDATE products SET stock_quantity = " + currentStock + " WHERE id = " + itemId, function(error, results){
+          if (error) throw error;
+          console.log("You have purchased $" + price*itemQuantity + " worth of items. There are now " + currentStock + " left.")
+          connection.end();
+        });
+      }
+    });
   });
+};
